372b6b8055ec82ce10a9a413a285805d8ba6b054
Baran Karaaslan <barankaraaslan@protonmail.com>
2022-05-07 23:23:49 +0300

    simplify phases, no dependency between phases

--- depman/main.py
+++ depman/main.py
@@ -9,20 +9,12 @@ def package():
 def install():
     print('installing')
 
-class Phase:
-    def __init__(self, action, next=None):
-        self.action = action
-        self.next = next
-
 default_phases = {
-    'info': Phase(info, 'package'),
-    'package': Phase(package, 'install'),
-    'install': Phase(install)
+    'info': info,
+    'package': package,
+    'install': install
 }
-default_initial_phase = 'info'
 
-def run_depman(phases=default_phases, initial_phase=default_initial_phase):
-    key = initial_phase
-    while key:
-        phases[key].action()
-        key = phases[key].next
+def run_depman(phases=default_phases):
+    for phase_action in phases.values():
+        phase_action()
