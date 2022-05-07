3288ab0c16cc7d397b166f2e5112555eb1801c18
Baran Karaaslan <barankaraaslan@protonmail.com>
2022-05-08 00:22:50 +0300

    add packaging phase

--- test_project_1/build.py
+++ test_project_1/build.py
@@ -1,5 +1,7 @@
 from depman.main import run_depman
 from subprocess import run,check_output
+from shutil import copy
+from os import makedirs
 
 def build():
     run('g++ -c logger.cpp', shell=True, check=True)
@@ -10,10 +12,15 @@ def build_test():
 def test():
     check_output('./a.out', shell=True) == 'Logged by my beautiful library ::Hello World!\n'
 
+def package():
+    makedirs('package/lib', exist_ok=True)
+    copy('logger.o', 'package/lib/')
+
 phases = {
     'build': build,
     'build_test': build_test,
     'test': test,
+    'package': package,
 }
 
 run_depman(phases)
