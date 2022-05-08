013f1cbfbc2b62348eeb69282ba6fd18fc69f58f
Baran Karaaslan <barankaraaslan@protonmail.com>
2022-05-08 10:14:06 +0300

    add package info generation phase

--- test_project_1/build.py
+++ test_project_1/build.py
@@ -2,15 +2,14 @@ from depman.main import run_depman
 from subprocess import run, check_output
 from shutil import copy, copytree, rmtree
 from os import makedirs, remove
-
-version = '1.0.0'
-name = 'log4cpp'
+from configparser import ConfigParser
 
 def clean():
     try:
         rmtree('package')
         remove('a.out')
         remove('log4cpp.o')
+        remove('package_info.txt')
     except FileNotFoundError:
         pass
 
@@ -27,12 +26,22 @@ def package():
     makedirs('package/lib', exist_ok=True)
     copy('log4cpp.o', 'package/lib/')
 
+def generate_package_info():
+    config = ConfigParser()
+    config['info'] = {
+        'name': 'log4cpp',
+        'version': '1.0.0',
+    }
+    with open('package_info.txt', 'w') as _file:
+        config.write(_file)
+
 phases = {
     'clean': clean,
     'build': build,
     'build_test': build_test,
     'test': test,
     'package': package,
+    'generate_package_info': generate_package_info,
 }
 
 run_depman(phases)
