524fb5bf1f4bd4784bb3431686ff3d9ac9ec01eb
Baran Karaaslan <barankaraaslan@protonmail.com>
2022-05-08 09:54:14 +0300

    add clean phase and name project

--- test_project_1/build.py
+++ test_project_1/build.py
@@ -1,22 +1,34 @@
 from depman.main import run_depman
-from subprocess import run,check_output
-from shutil import copy
-from os import makedirs
+from subprocess import run, check_output
+from shutil import copy, copytree, rmtree
+from os import makedirs, remove
+
+version = '1.0.0'
+name = 'log4cpp'
+
+def clean():
+    try:
+        rmtree('package')
+        remove('a.out')
+        remove('log4cpp.o')
+    except FileNotFoundError:
+        pass
 
 def build():
-    run('g++ -c logger.cpp', shell=True, check=True)
+    run('g++ -c log4cpp.cpp', shell=True, check=True)
 
 def build_test():
-    run('g++ -I $PWD test/main.cpp -L $PWD logger.o', shell=True, check=True)
+    run('g++ -I $PWD test/main.cpp -L $PWD log4cpp.o', shell=True, check=True)
 
 def test():
     check_output('./a.out', shell=True) == 'Logged by my beautiful library ::Hello World!\n'
 
 def package():
     makedirs('package/lib', exist_ok=True)
-    copy('logger.o', 'package/lib/')
+    copy('log4cpp.o', 'package/lib/')
 
 phases = {
+    'clean': clean,
     'build': build,
     'build_test': build_test,
     'test': test,
--- test_project_1/test/main.cpp
+++ test_project_1/test/main.cpp
@@ -1,4 +1,4 @@
-#include "logger.h"
+#include "log4cpp.h"
 
 int main() {
     log("Hello World!");
